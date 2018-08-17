d65e955439fb1ee455748329a9304e01e03368f0
Ciel <imwithye@gmail.com>
2018-08-17 11:30:41 +0800

    add depoly.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+hugo
+git checkout master
+mv public /tmp/hugo.vrcollab.com
+rm -rf *
+mv /tmp/hugo.vrcollab.com/* .
+rm -rf /tmp/hugo.vrcollab.com
+
+message="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then message="$1"
+fi
+git add .
+git commit --allow-empty -m "$message"
+git push origin master -f
+git checkout src
